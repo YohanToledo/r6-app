35e3a489abedfbed990cd53ed7009da2bd520b63
Yohan Gabriel Toledo Bernart <yohan.gabriel.bernart@gmail.com>
2022-12-20 01:26:18 +0000

    fix: add cors

--- api/src/app.js
+++ api/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 const operatorsController = require("./components/operators/operators.controller");
 
 app.use(bodyParser.json());
+app.use(cors());
 
 //Rotas
 app.use("/api/v1/operators", operatorsController);
