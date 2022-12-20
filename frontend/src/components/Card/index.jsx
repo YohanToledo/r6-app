3341b0c1ae213bae829b9974625936c96025d7a4
Yohan Gabriel Toledo Bernart <yohan.gabriel.bernart@gmail.com>
2022-12-20 18:09:01 +0000

    feat: add lazy load and loading icon to images

--- frontend/src/components/Card/index.jsx
+++ frontend/src/components/Card/index.jsx
@@ -1,18 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import Modal from "../Modal";
 import "./Card.css";
+import { BiLoaderAlt } from "react-icons/bi";
 
 const Card = ({ operatorInfo }) => {
   const [show, setShow] = useState(false);
 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = operatorInfo.imageUrl;
+    img.onprogress = () => setLoading(true);
+    img.onload = () => {
+      setLoading(false);
+    };
+  }, [operatorInfo.imageUrl]);
+
   return (
     <div className={`card-container ${operatorInfo.team}`}>
-      <img
-        onClick={() => setShow(true)}
-        className="operator-iamge"
-        src={operatorInfo.imageUrl}
-        alt=""
-      />
+      {loading ? (
+        <div className="image-load">
+          <BiLoaderAlt className="load-icon" />
+        </div>
+      ) : (
+        <LazyLoadImage
+          className="operator-iamge"
+          onClick={() => setShow(true)}
+          src={operatorInfo.imageUrl}
+          width={120}
+          height={220}
+          effect="blur"
+        />
+      )}
       <h3 className="op-name">
         <b>{operatorInfo.operator.toUpperCase()}</b>
       </h3>
